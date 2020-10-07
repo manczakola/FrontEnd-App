198445761547322e882ffe5a03a9b8804e1eefd3
Ola Manczak <manczakola@gmail.com>
2020-10-07 20:47:17 +0200

    add the checked options

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const nameTile = document.querySelectorAll('.nameTile');
 const hideTile = document.querySelectorAll('.hideTile');
+const checkboxes = [...document.querySelectorAll('input[type=checkbox]')];
+
 
 function showList(e) {
     const thisList = this.parentNode.children[1];
@@ -7,7 +9,31 @@ function showList(e) {
 
 
 }
+let i = 0;
+
+function doneTile(e) {
+    if (e.target.checked) {
+        i++
+    } else {
+        i--
+    }
+    console.log(i);
+    const thisNameTile = this.parentNode.parentNode.parentNode.parentNode.children[0];
+    if (i === checkboxes.length) {
+        thisNameTile.classList.add('done');
+
+    } else if (i < checkboxes.length) {
+        thisNameTile.classList.remove('done');
+    }
+
+}
+
+
+
 
 nameTile.forEach(tile => {
     tile.addEventListener('click', showList)
+});
+checkboxes.forEach(check => {
+    check.addEventListener('change', doneTile)
 })
