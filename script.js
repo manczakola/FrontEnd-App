e6d04e0fd72a69c267780cec0ac1384fb91f1e82
Ola Manczak <manczakola@gmail.com>
2020-10-14 20:33:06 +0200

    update files

--- script.js
+++ script.js
@@ -3,48 +3,51 @@ const nameTile = document.querySelectorAll('.nameTile');
 const hideTile = document.querySelectorAll('.hideTile');
 const checkboxes = [...document.querySelectorAll('input[type=checkbox]')];
 const internetChceckboxes = tiles[0].querySelectorAll('input[type=checkbox]');
+const percent = document.querySelector("div.percent");
 
-
+// Functions
 
 function showList(e) {
+    // Display list item when click name of tile
+
     const thisList = this.parentNode.children[1];
     thisList.classList.toggle('display')
 
 
 }
-let i = 0;
+
 
 function doneTile(e) {
-    const thisTile = e.target.parentNode.parentNode.parentNode.parentNode;
-
+    const thisTile = e.path[4];
     const thisNameTile = thisTile.children[0];
+    const thisHideTile = thisTile.children[1];
     const index = e.target.dataset.index;
     const thisCheckboxes = [...tiles[index - 1].querySelectorAll('input[type=checkbox]')];
     const checkedCheckboxes = [...tiles[index - 1].querySelectorAll('input[type=checkbox]:checked')].length;
-
-    // console.log(thisTile, thisNameTile, index, thisCheckboxes, thisCheckboxes.length, thisCheckboxes.checked);
-    // console.log(thisCheckboxes, checkedCheckboxes);
+    const thisPercent = [...tiles[index - 1].querySelectorAll('.percent')];
 
 
+    //Add done class to to nameTile if every checkbox is checked
 
     if (checkedCheckboxes == thisCheckboxes.length) {
         thisNameTile.classList.add('done');
+        thisHideTile.classList.remove('display');
     } else {
         thisNameTile.classList.remove('done');
     };
+    // Add percent of the bottom of tile
+    const percentOfTile = Math.floor((checkedCheckboxes / thisCheckboxes.length) * 100) + '%';
 
-    const percentOfTile = Math.floor((checkedCheckboxes / thisCheckboxes.length) * 100);
-    if (checkedCheckboxes > 0) {
+    thisPercent.forEach(p => {
+        (checkedCheckboxes > 0) ? p.textContent = percentOfTile: p.textContent = '';
+    });
 
-        nameTile.textContent = +`${percentOfTile}%`;
-    }
+
 
 }
 
 
-
-
-
+//Events
 nameTile.forEach(tile => {
     tile.addEventListener('click', showList)
 });
